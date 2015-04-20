c5041e593ff52c425f73a8f0a4ece214a23939f0
Aayush Kumar <iaayush.stevejobs@gmail.com>
2015-04-21 00:32:54 +0530

    Adding Existing Username check

--- application/models/model_sign_up.php
+++ application/models/model_sign_up.php
@@ -10,29 +10,53 @@ class Model_Sign_Up extends CI_Model {
 
     public function addUser($username, $password) {
 
-        $arrayForJSONObject = array(
+        if($this->checkForExistingUsers($username))
+            return 0;
+        else {
+            $arrayForJSONObject = array(
 
-            "username" => $username,
-            "password" => $password
-        );
+                "username" => $username,
+                "password" => $password
+            );
 
-        $dataString = json_encode($arrayForJSONObject);
-        $curlSession = curl_init(""); // URL to be updated for Adding Users into Database
+            $dataString = json_encode($arrayForJSONObject);
+            $curlSession = curl_init(""); // URL to be updated for Adding Users into Database
 
-        curl_setopt($curlSession, CURLOPT_CUSTOMREQUEST,"POST");
-        curl_setopt($curlSession, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-        curl_setopt($curlSession, CURLOPT_POSTFIELDS, $dataString);
-        curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($curlSession, CURLOPT_CUSTOMREQUEST,"POST");
+            curl_setopt($curlSession, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+            curl_setopt($curlSession, CURLOPT_POSTFIELDS, $dataString);
+            curl_setopt($curlSession, CURLOPT_RETURNTRANSFER, true);
 
-        curl_setopt($curlSession, CURLOPT_HTTPHEADER, array(
-                'Content-Type: application/json',
-                'Content-Length: ' . strlen($dataString))
-        );
+            curl_setopt($curlSession, CURLOPT_HTTPHEADER, array(
+                    'Content-Type: application/json',
+                    'Content-Length: ' . strlen($dataString))
+            );
 
-        $json_string = curl_exec($curlSession);
-        $resultCode = curl_getinfo($curlSession, CURLINFO_HTTP_CODE);
+            $json_string = curl_exec($curlSession);
+            $resultCode = curl_getinfo($curlSession, CURLINFO_HTTP_CODE);
 
-        if($resultCode == '200')
+            if($resultCode == '200')
+                return 1;
+            else
+                return 2;
+        }
+    }
+
+    public function checkForExistingUsers($username) {
+
+        $existingUsers = curl_init(""); // URL to get all the list of Users in the database
+
+        curl_setopt($existingUsers, CURLOPT_CUSTOMREQUEST,"GET");
+        curl_setopt($existingUsers, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+        curl_setopt($existingUsers, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($existingUsers, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+
+        $json_string = curl_exec($existingUsers);
+
+        $res_array = json_decode($json_string);
+
+        // Searching for username in the decoded JSON string into array
+        if(array_key_exists($username,$res_array))
             return true;
         else
             return false;
